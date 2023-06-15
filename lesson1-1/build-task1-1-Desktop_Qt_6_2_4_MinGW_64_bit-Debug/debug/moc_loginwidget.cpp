962e1e2ee987c3d3f81dd7374821c47d4ae65db4
delong34 <delong@bk.ru>
2023-06-15 09:20:00 +0300

    sam spam2

---
+++ lesson1-1/build-task1-1-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/debug/moc_loginwidget.cpp
@@ -0,0 +1,164 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'loginwidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.4)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../task1-1/loginwidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'loginwidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.4. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_LoginWidget_t {
+    const uint offsetsAndSize[12];
+    char stringdata0[73];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_LoginWidget_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_LoginWidget_t qt_meta_stringdata_LoginWidget = {
+    {
+QT_MOC_LITERAL(0, 11), // "LoginWidget"
+QT_MOC_LITERAL(12, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(34, 0), // ""
+QT_MOC_LITERAL(35, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(59, 5), // "color"
+QT_MOC_LITERAL(65, 7) // "opacity"
+
+    },
+    "LoginWidget\0on_pushButton_clicked\0\0"
+    "on_pushButton_2_clicked\0color\0opacity"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_LoginWidget[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       2,   28, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   26,    2, 0x08,    3 /* Private */,
+       3,    0,   27,    2, 0x08,    4 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // properties: name, type, flags
+       4, QMetaType::QColor, 0x00015103, uint(-1), 0,
+       5, QMetaType::Float, 0x00015103, uint(-1), 0,
+
+       0        // eod
+};
+
+void LoginWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<LoginWidget *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_pushButton_clicked(); break;
+        case 1: _t->on_pushButton_2_clicked(); break;
+        default: ;
+        }
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<LoginWidget *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QColor*>(_v) = _t->color(); break;
+        case 1: *reinterpret_cast< float*>(_v) = _t->opacity(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<LoginWidget *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setColor(*reinterpret_cast< QColor*>(_v)); break;
+        case 1: _t->setOpacity(*reinterpret_cast< float*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    } else if (_c == QMetaObject::BindableProperty) {
+    }
+#endif // QT_NO_PROPERTIES
+    (void)_a;
+}
+
+const QMetaObject LoginWidget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_LoginWidget.offsetsAndSize,
+    qt_meta_data_LoginWidget,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_LoginWidget_t
+, QtPrivate::TypeAndForceComplete<QColor, std::true_type>, QtPrivate::TypeAndForceComplete<float, std::true_type>, QtPrivate::TypeAndForceComplete<LoginWidget, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *LoginWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *LoginWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_LoginWidget.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int LoginWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::BindableProperty
+            || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
